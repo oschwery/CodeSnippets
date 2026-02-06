99ffc17ab1e3647b0f43efa1e00e3b5028f9eb06
oschwery <schwery.macroevo@pm.me>
2026-02-06 04:45:52 -0500

    simplified and avoid error for usage line

--- stateplot_standalone.R
+++ stateplot_standalone.R
@@ -5,13 +5,11 @@
 # nrates is the number of rate categories (I gotta automate that sometime)
 # type is the plot type ('fan', 'phylogram', etc.)
 
-stateplot <- function(rates, nrates, cols=NULL, type, tiplab=FALSE, cex=0.5) {
+stateplot <- function(rates, nrates, col=NULL, type, tiplab=FALSE, cex=0.5) {
     plot(rates$phy, type=type, show.tip.label=tiplab, label.offset=1, cex=cex)
-    if (is.null(cols)) {
+    if (is.null(col)) {
         col <- c("darkcyan", "gold", "maroon2", "darkorchid", "seagreen2")  # the colours need to be in the same order as the trait states
-    } else {
-        col <- cols
-    }
+    } 
     # tiplabels(pch=22, bg=col[as.numeric(as.factor(discretes[,2]))], cex=2, adj=1)  # we get the tip state coloured based on what the state is in the original data (with some subsetting etc)
     tiplabels(pie = rates$tip.mat[, 2:(nrates+1)], piecol = col, cex = 0.25, adj=1)  # we get the node states coloured based on the reconstruction $lik.anc is the likelihood values for each of the different trait states at 
     nodelabels(pie = rates$node.mat[, 2:(nrates+1)], piecol = col, cex = 0.25)  # we get the node states coloured based on the reconstruction $lik.anc is the likelihood values for each of the different trait states at 
@@ -22,4 +20,4 @@ stateplot <- function(rates, nrates, cols=NULL, type, tiplab=FALSE, cex=0.5) {
 
 # Usage:
 
-stateplot(four.rate.recon, 4, "phylogram")
+#stateplot(four.rate.recon, 4, "phylogram")
